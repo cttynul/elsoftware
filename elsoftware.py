f9b98be307cf87011acef16bd28faa2e530f4839
cttynul <cttynul@outlook.com>
2022-02-03 19:49:14 +0100

    last versionz

--- elsoftware.py
+++ elsoftware.py
@@ -1,130 +1,113 @@
-import requests
+import requests, re, json
 import pandas as pd
 
-codice_segretissimo = ["-6", "1614489243000", "1643664948000"]
-years = ["2019-20", "2020-21", "2021-22"]
-years_compact = ["19", "20", "21"]
+CONFIG_FILE = "config.json"
+
+with open(CONFIG_FILE) as f:
+	data = json.load(f)
+	try: codice_segretissimo = data["codice_segretissimo"]
+	except: codice_segretissimo = ["-6", "1614489243000", "1643664948000"]
+	try: years = data["years"]
+	except: years = ["2019-20", "2020-21", "2021-22"]
+	try: position = data["position"]
+	except: position = ["Portieri", "Difensori", "Centrocampisti", "Attaccanti"]
+
+position_compact = [p[:1] for p in position]
+
+logo = '''
+
+      _   _____        __ _                          
+     | | /  ___|      / _| |                         
+  ___| | \ `--.  ___ | |_| |___      ____ _ _ __ ___ 
+ / _ \ |  `--. \/ _ \|  _| __\ \ /\ / / _` | '__/ _ \\
+|  __/ | /\__/ / (_) | | | |_ \ V  V / (_| | | |  __/
+ \___|_| \____/ \___/|_|  \__| \_/\_/ \__,_|_|  \___|
+                                                     
+    FantaHack                                - cttynul          
+'''
+
+print(logo + "\nSettings:\n\tConfig File: " + CONFIG_FILE + "\n\tAnni: " + str(years) + "\n\tPosition Compact:" + str(position_compact) + "\n")
+
 #years = ["2015-16", "2016-17", "2017-18"] Solo per ricordo
+
+try: partite_giocate = int(re.findall('<td>([0-9]+)<\/td>', requests.get("https://sport.virgilio.it/calcio/serie-a/classifica/").text)[0])
+except: partite_giocate = 38
 
 open('Quotazioni_Fantacalcio_Ruoli_Fantagazzetta.xlsx', 'wb').write(requests.get("https://www.fantacalcio.it/Servizi/Excel.ashx?type=0&r=1&t=" + codice_segretissimo[-1], allow_redirects=True).content)
 for y in years: open('Statistiche_Fantacalcio_' + y + '_Fantagazzetta.xlsx', 'wb').write(requests.get("https://www.fantacalcio.it/Servizi/Excel.ashx?type=2&r=1&t=" + codice_segretissimo[years.index(y)] + "&s=" + y, allow_redirects=True).content)
 
 dataset_quotazioni = pd.read_excel("Quotazioni_Fantacalcio_Ruoli_Fantagazzetta.xlsx",header = 1)
 
-#debug 
-#list(dataset_quotazioni.columns.values)
-#['Id', 'R', 'Nome', 'Squadra', 'Qt. A', 'Qt. I', 'Diff.']
-
 #droppo le colonne che non voglio utilizzare nel dataset finale
-
-clean_quot = ['Id', 'Squadra', 'Qt. A', 'Diff.']
+clean_quot = ['Id', 'Qt. A', 'Diff.']
 for e in clean_quot:
-    try: dataset_quotazioni = dataset_statistiche1.drop(e, 1)
+    try: dataset_quotazioni = dataset_quotazioni.drop(labels=e, axis=1)
     except: pass
-
-#debug
-#list(dataset_quotazioni.columns.values)
-#['Nome', 'Qt. I']
 
 dataset_statistiche1 = pd.read_excel("Statistiche_Fantacalcio_" + years[0] + "_Fantagazzetta.xlsx",header = 1)
 
-#debug 
-#list(dataset_statistiche1.columns.values)
-#to_be_removed = ['Id', 'R', 'Nome', 'Squadra', 'Pg_' + years_compact[0], 'Mv_' + years_compact[0], 'Mf_' + years_compact[0], 'Gf_' + years_compact[0], 'Gs_' + years_compact[0], 'Rp_' + years_compact[0], 'Rc_' + years_compact[0], 'R+_' + years_compact[0], 'R-_' + years_compact[0], 'Ass_' + years_compact[0], 'Asf_' + years_compact[0], 'Amm_' + years_compact[0], 'Esp_' + years_compact[0], 'Au_' + years_compact[0]]
-
 #droppo le colonne che non voglio utilizzare nel dataset finale
-#Cancello la media voto, considero la media voto con bonus/malus
+#cancello la media voto, considero la media voto con bonus/malus
 to_be_removed = ['Id', 'R', 'Squadra', 'Mv', 'Gf', 'Gs', 'Rp', 'Rc', 'R+', 'R-', 'Ass', 'Asf', 'Amm', 'Esp', 'Au']
 
 for e in to_be_removed: 
-    try: dataset_statistiche1 = dataset_statistiche1.drop(e, 1)
+    try: dataset_statistiche1 = dataset_statistiche1.drop(labels=e, axis=1)
     except: pass
 
-    
-#list(dataset_statistiche1.columns.values)
-
-#ruolo, nome, partite_giocate,media_fantacalcio
-#['R', 'Nome', 'Pg_' + years_compact[0], 'Mf_' + years_compact[0]]
-
 dataset_statistiche2 = pd.read_excel("Statistiche_Fantacalcio_" + years[1] + "_Fantagazzetta.xlsx",header = 1)
-#to_be_removed = ['Id', 'R', 'Nome', 'Squadra', 'Pg_' + years_compact[1], 'Mv_' + years_compact[1], 'Mf_' + years_compact[1], 'Gf_' + years_compact[1], 'Gs_' + years_compact[1], 'Rp_' + years_compact[1], 'Rc_' + years_compact[1], 'R+_' + years_compact[1], 'R-_' + years_compact[1], 'Ass_' + years_compact[1], 'Asf_' + years_compact[1], 'Amm_' + years_compact[1], 'Esp_' + years_compact[1], 'Au_' + years_compact[1]]
 for e in to_be_removed: 
-    try: dataset_statistiche2 = dataset_statistiche2.drop(e, 1)
+    try: dataset_statistiche2 = dataset_statistiche2.drop(labels=e, axis=1)
     except: pass
 
 dataset_statistiche3 = pd.read_excel("Statistiche_Fantacalcio_" + years[2] + "_Fantagazzetta.xlsx",header = 1)
-#to_be_removed = ['Id', 'R', 'Nome', 'Squadra', 'Pg_' + years_compact[2], 'Mv_' + years_compact[2], 'Mf_' + years_compact[2], 'Gf_' + years_compact[2], 'Gs_' + years_compact[2], 'Rp_' + years_compact[2], 'Rc_' + years_compact[2], 'R+_' + years_compact[2], 'R-_' + years_compact[2], 'Ass_' + years_compact[2], 'Asf_' + years_compact[2], 'Amm_' + years_compact[2], 'Esp_' + years_compact[2], 'Au_' + years_compact[2]]
 for e in to_be_removed: 
-    try: dataset_statistiche3 = dataset_statistiche3.drop(e, 1)
+    try: dataset_statistiche3 = dataset_statistiche3.drop(labels=e, axis=1)
     except: pass
 
 # converti in pd
 df1 = pd.DataFrame(dataset_quotazioni)
-df2 = pd.DataFrame(dataset_statistiche1)
-df3 = pd.DataFrame(dataset_statistiche2)
-df4 = pd.DataFrame(dataset_statistiche3)
-
-print(df1)
-print(df2)
-print(df3)
-print(df4)
-
-
+df2 = (pd.DataFrame(dataset_statistiche1).rename(columns = {'Pg': 'Partite Giocate ' + years[0], 'Pg': 'Partite Giocate ' + years[0]}, inplace = False)).rename(columns = {'Mf': 'Fantamedia ' + years[0], 'Mf': 'Fantamedia ' + years[0]}, inplace = False)
+df3 = (pd.DataFrame(dataset_statistiche2).rename(columns = {'Pg': 'Partite Giocate ' + years[1], 'Pg': 'Partite Giocate ' + years[1]}, inplace = False)).rename(columns = {'Mf': 'Fantamedia ' + years[1], 'Mf': 'Fantamedia ' + years[1]}, inplace = False)
+df4 = (pd.DataFrame(dataset_statistiche3).rename(columns = {'Pg': 'Partite Giocate ' + years[2], 'Pg': 'Partite Giocate ' + years[2]}, inplace = False)).rename(columns = {'Mf': 'Fantamedia ' + years[2], 'Mf': 'Fantamedia ' + years[2]}, inplace = False)
 dataset = df1.merge(df2, on="Nome").merge(df3, on="Nome").merge(df4, on="Nome")
 result = dataset.sort_values(by='Qt. I')
 
-print(dataset)
+#print(dataset)
 
 media_giocatori = []
 for index, row in dataset.iterrows():
-	if  row.Pg_x > 0:	 
-		media_pesata_partite_giocate = (row.Pg_x/38 * row.Mf_x)*0.20 + (row.Pg_y/38 * row.Mf_y)*0.80
+	if  row['Partite Giocate ' + years[0]] > 0:	 
+		media_pesata_partite_giocate = (row['Partite Giocate ' + years[0]]/38 * row['Fantamedia ' + years[0]])*0.20 + (row['Partite Giocate ' + years[1]]/38 * row['Fantamedia ' + years[1]])*0.60 + (row['Partite Giocate ' + years[2]]/partite_giocate * row['Fantamedia ' + years[2]])*1.20
 		media_giocatori.append(media_pesata_partite_giocate)
 	else:
 		media_giocatori.append(0)
 
-dataset["mediaGiocatori"] = media_giocatori
+dataset["Fattore Fantahack"] = media_giocatori
 
 media = []
 
 for index, row in dataset.iterrows():
-	if  row.mediaGiocatori > 0:	 
-		media.append(row.mediaGiocatori/row["Qt. I"])
+	if  row["Fattore Fantahack"] > 0:	 
+		media.append(row["Fattore Fantahack"]/row["Qt. I"])
 	else:
 		media.append(0)
-dataset["media"] = media
-result = dataset.sort_values(by="media")
+dataset["Rapporto Fattore Quota"] = media
+result = dataset.sort_values(by="Rapporto Fattore Quota")
 
 total = []
 for index, row in dataset.iterrows():
-	if  row.mediaGiocatori > 0:	 
-		total.append(row.mediaGiocatori*row.media*row['Mf'])
+	if  row["Fattore Fantahack"] > 0:	 
+		total.append((row["Fattore Fantahack"]*row["Rapporto Fattore Quota"]*row['Fantamedia ' + years[2]]) / 10)
 	else:
 		total.append(0)
 
-dataset["convenienza"] = total
-result = dataset.sort_values(by="convenienza",ascending=False)
-
+dataset["Convenienza"] = total
+result = dataset.sort_values(by="Convenienza",ascending=False)
 print(result)
 
-with open("dataset_att.csv", "w") as f:
-	df = result[result["R"] == "A"]
-	f.write(df.to_string())
+with pd.ExcelWriter("Fantahack.xlsx") as writer:
+	for p in position_compact: result[result["R"] == p].to_excel(writer, sheet_name=position[position_compact.index(p)], index=False)
 
-with open("dataset_cen.csv", "w") as f:
-	df = result[result["R"] == "C"]
-	f.write(df.to_string())
-
-with open("dataset_dif.csv", "w") as f:
-	df = result[result["R"] == "D"]
-	f.write(df.to_string())
-
-with open("dataset_por.csv", "w") as f:
-	df = result[result["R"] == "P"]
-	f.write(df.to_string())
-
-with open("dataset.csv","w+") as f:
-	f.write(result.to_string() )
 '''
 #f.write(media_giocatori.to_string())
 
